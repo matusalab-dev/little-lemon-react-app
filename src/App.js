8c7c0dea580da11522d37af53cf8d2669db4d444
matusala <mtslabrh97@gmail.com>
2023-05-29 14:21:09 +0300

    wrap the home-page text inside the H1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Home-page</div>;
+  return (
+    <div className="App">
+      <h1>Home-page</h1>
+    </div>
+  );
 }
 
 export default App;
